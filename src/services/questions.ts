befc73cf8b35c2aff3788523aada5e57f114efa1
Rayzler <cesar281103@gmail.com>
2024-05-06 18:01:24 -0600

    URL fetch updated

--- src/services/questions.ts
+++ src/services/questions.ts
@@ -1,6 +1,6 @@
 ﻿export async function getQuestions(limit: number) {
     await delay(250);
-    const res = await fetch("http://localhost:5173/data.json");
+    const res = await fetch("https://my-js-quiz.vercel.app/data.json");
     const data = await res.json();
     return data.sort(() => Math.random() - 0.5).slice(0, limit);
 }
